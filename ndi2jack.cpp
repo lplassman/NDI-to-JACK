c549df6c3dca32ecec243184cbfe982e2650d963
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-21 17:36:53 -0500

    added multiple source selection form http interface

--- ndi2jack.cpp
+++ ndi2jack.cpp
@@ -16,16 +16,28 @@
 #include <ctime>
 #include <atomic>
 #include <unistd.h>
+#include <fstream> //for reading and writing preset file
 #include <mongoose.h>
+#include "mjson.h"
 #include <thread>
 
 #include <Processing.NDI.Lib.h>
 #include <jack/jack.h>
 
+NDIlib_find_create_t NDI_find_create_desc; /* Default settings for NDI find */
+NDIlib_find_instance_t pNDI_find;
+const NDIlib_source_t* p_sources = NULL;
+struct mg_mgr mgr;   
+
+
+//Function Definitions
 int process_callback(jack_nframes_t x, void *p);
+std::string convertToString(char* a);
+
+
 
 struct receive_audio {
- receive_audio(const NDIlib_source_t& source, const char *client_name="NDI_recv"); //constructor
+ receive_audio(const char* source, const char *client_name="NDI_recv"); //constructor
  ~receive_audio(void); //destructor 
  public:
   int process(jack_nframes_t nframes);
@@ -34,30 +46,10 @@ struct receive_audio {
   NDIlib_framesync_instance_t m_pNDI_framesync; //NDI framesync
   jack_port_t *out_port1, *out_port2;
   jack_client_t *jack_client;
-  std::thread m_receive_thread; // The thread to run
 	std::atomic<bool> m_exit;	// Are we ready to exit
   void receive(void); // This is called to receive frames		
   static void jack_shutdown(void *arg); //This is called when JACK is shutdown
-  
 };
-
-static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data){
-  if (ev == MG_EV_HTTP_MSG){
-  struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-   if(mg_http_match_uri(hm, "/ws")){ //upgrade to WebSocket
-      mg_ws_upgrade(c, hm, NULL);
-   }else if(mg_http_match_uri(hm, "/rest")) { //handle REST events
-      mg_http_reply(c, 200, "", "{\"result\": %d}\n", 123);
-   }else{ // Serve static files
-      struct mg_http_serve_opts opts = {.root_dir = "."};
-      mg_http_serve_dir(c, (mg_http_message*)ev_data, &opts);
-   }
-  }else if (ev == MG_EV_WS_MSG){
-    // Got websocket frame. Received data is wm->data. Echo it back!
-    struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
-    mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
-  }
-}
 
 int receive_audio::process(jack_nframes_t nframes){
   jack_default_audio_sample_t *out1, *out2;
@@ -93,10 +85,9 @@ void receive_audio::jack_shutdown(void *arg){
 }
 
 //Constructor
-receive_audio::receive_audio(const NDIlib_source_t& source, const char *client_name): m_pNDI_recv(NULL), m_exit(false), out_port1(NULL), out_port2(NULL){
-  printf("Starting Receiver for %s\n", source.p_ndi_name);
+receive_audio::receive_audio(const char* source, const char *client_name): m_pNDI_recv(NULL), m_pNDI_framesync(NULL), m_exit(false), jack_client(NULL), out_port1(NULL), out_port2(NULL){
+  //printf("Starting Receiver for %s\n", source);
   const char **ports;
-  //const char *client_name;
   const char *server_name = NULL;
   jack_options_t options = JackNullOption;
   jack_status_t status;
@@ -170,24 +161,16 @@ receive_audio::receive_audio(const NDIlib_source_t& source, const char *client_n
 	assert(m_pNDI_recv);
 
   // Use a frame-syncronizer to ensure that the audio is dynamically resampled
-  m_pNDI_framesync = NDIlib_framesync_create(m_pNDI_recv);
-  // Start a thread to receive frames
-	m_receive_thread = std::thread(&receive_audio::receive, this);
+  m_pNDI_framesync = NDIlib_framesync_create(m_pNDI_recv); //starts in its own thread
 }
 
 // Destructor
 receive_audio::~receive_audio(void){	// Wait for the thread to exit
 	m_exit = true;
-	m_receive_thread.join();
-	
+	jack_client_close(jack_client);
 	// Destroy the receiver
+  NDIlib_framesync_destroy(m_pNDI_framesync);
 	NDIlib_recv_destroy(m_pNDI_recv);
-}
-
-void receive_audio::receive(void){
-  while(!m_exit){
-   sleep(1);
-  }
 }
 
 /**
@@ -198,53 +181,186 @@ int process_callback(jack_nframes_t x, void *p){
  return static_cast<receive_audio*>(p)->process(x); 
 }
 
+static const int no_receivers = 30; //max number of receivers
+receive_audio* p_receivers[no_receivers] = { 0 };
+std::string ndi_running_name[no_receivers] = { "" };
+
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data){
+  if(ev == MG_EV_WS_OPEN){
+    c->label[0] = 'W';  // Mark this connection as an established WS client
+  }
+  if (ev == MG_EV_HTTP_MSG){
+  struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+  struct mg_connection *c2 = mgr.conns;
+   if(mg_http_match_uri(hm, "/ws")){ //upgrade to WebSocket
+      mg_ws_upgrade(c, hm, NULL);
+   }else if(mg_http_match_uri(hm, "/rest")) { //handle REST events
+      mg_http_reply(c, 200, "", "{\"result\": %d}\n", 123);
+   }else{ // Serve static files
+      struct mg_http_serve_opts opts = {.root_dir = "/opt/ndi2jack/assets/"};
+      mg_http_serve_dir(c, (mg_http_message*)ev_data, &opts);
+   }
+  }else if (ev == MG_EV_WS_MSG){
+    // Got websocket frame. Received data is wm->data. Echo it back!
+    struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+    //std::cout << "WebSocket: " << wm->data.ptr << std::endl;
+    char prefix_buf[100];
+    char action_buf[100];
+    mjson_get_string(wm->data.ptr, wm->data.len, "$.prefix", prefix_buf, sizeof(prefix_buf)); //get prefix
+    mjson_get_string(wm->data.ptr, wm->data.len, "$.action", action_buf, sizeof(action_buf)); //get action
+    std::string prefix_string = convertToString(prefix_buf);
+    std::string action_string = convertToString(action_buf);
+    if(prefix_string == "refresh"){
+     if(action_string == "refresh"){
+
+      uint32_t no_sources = 0; 
+      p_sources = NDIlib_find_get_current_sources(pNDI_find, &no_sources);
+      std::string discover_json;
+      std::string source_json = "";
+      discover_json = "{\"prefix\":\"discover_source\",\"action\":\"display\",\"source_list\":{";
+      for(uint32_t i = 0; i < no_sources; i++){
+       std::string ndi_string = p_sources[i].p_ndi_name;
+       std::string source_id = std::to_string(i); 
+       int conflict = 0;
+       for(uint32_t i = 0; i < no_receivers; i++){ //check for conflicts
+        if((ndi_running_name[i] == ndi_string)&&(conflict == 0)){
+         conflict = 1; //found conflict with a name that is already stored - already running this receiver
+        }  
+       }
+       if(conflict == 0){ //since this is not running on a receiver - display
+        std::cout << "Source IP: " << p_sources[i].p_url_address << std::endl;
+        if(source_json == ""){
+         source_json += "\""+source_id + "\":{\"name\":\""+ndi_string+"\"}";  
+        }else{
+         source_json += ",\""+source_id + "\":{\"name\":\""+ndi_string+"\"}";  
+        }
+       }
+      }
+      discover_json += source_json;
+      discover_json += "}";
+      discover_json += "}";
+      const char* pub_json1 = discover_json.c_str();
+      for (struct mg_connection *c2 = mgr.conns; c2 != NULL; c2 = c2->next) { //traverse over all client connections
+       if (c2->label[0] == 'W'){ //make sure it is a websocket connection
+        mg_ws_send(c2, pub_json1, strlen(pub_json1), WEBSOCKET_OP_TEXT);
+       }
+      }
+
+      std::string connected_json;
+      source_json = "";
+      connected_json = "{\"prefix\":\"playing_source\",\"action\":\"display\",\"source_list\":{";
+      for(uint32_t i = 0; i < no_receivers; i++){
+       if(ndi_running_name[i] != ""){ //make sure receiver is not empty
+        std::string source_id = std::to_string(i); 
+        if(source_json == ""){
+         source_json += "\""+source_id + "\":{\"name\":\""+ndi_running_name[i]+"\"}";  
+        }else{
+         source_json += ",\""+source_id + "\":{\"name\":\""+ndi_running_name[i]+"\"}";  
+        }
+       }
+      }
+      connected_json += source_json;
+      connected_json += "}";
+      connected_json += "}";
+      const char* pub_json2 = connected_json.c_str();
+      for (struct mg_connection *c2 = mgr.conns; c2 != NULL; c2 = c2->next) { //traverse over all client connections
+       if (c2->label[0] == 'W'){ //make sure it is a websocket connection
+        mg_ws_send(c2, pub_json2, strlen(pub_json2), WEBSOCKET_OP_TEXT);
+       }
+      }
+     } 
+    }
+
+    if(prefix_string == "connect_source"){
+     int source_id = std::stoi(action_string);
+     int stored = 0;
+     int receiver_id = 0;
+     int conflict = 0;
+     std::string ndi_string = p_sources[source_id].p_ndi_name;
+     for(uint32_t i = 0; i < no_receivers; i++){ //check for conflicts
+      if((ndi_running_name[i] == ndi_string)&&(conflict == 0)){
+       conflict = 1; //found conflict with a name that is already stored - already running this receiver
+      }  
+     }
+     if(conflict == 0){
+      for(uint32_t i = 0; i < no_receivers; i++){
+       if(stored == 0){
+        if(ndi_running_name[i] == ""){ //empty string array
+         ndi_running_name[i] = ndi_string;
+         std::cout << "ID: " << i << std::endl;
+         stored = 1;
+         receiver_id = i;
+        } 
+       }
+      }
+      p_receivers[receiver_id] = new receive_audio(p_sources[source_id].p_ndi_name, "NDI_recv"); 
+     }else{
+      //std::cout << "Receiver already running for:  " << p_sources[source_id].p_ndi_name << std::endl; 
+     }
+    }
+
+    if(prefix_string == "disconnect_source"){
+     int source_id = std::stoi(action_string);
+     delete p_receivers[source_id]; //delete receiver
+     ndi_running_name[source_id] = ""; //update the running receiver
+    }
+
+    if(prefix_string == "save_streams"){
+     std::ofstream preset_file("presets.txt");
+     for(uint32_t i = 0; i < no_receivers; i++){
+      if(ndi_running_name[i] != ""){ //make sure a receiver is stored before trying to save in file
+      preset_file << ndi_running_name[i];
+      preset_file << std::endl;
+      }
+     }
+     preset_file.close();
+    }
+    
+  }
+}
+
 int main (int argc, char *argv[]){
 
-  if (!NDIlib_initialize()){
-		// Cannot run NDI. Most likely because the CPU is not sufficient (see SDK documentation).
-		// you can check this directly with a call to NDIlib_is_supported_CPU()
-		printf("Cannot run NDI.");
-		return 0;
+  if(!NDIlib_initialize()){	
+	 printf("Cannot run NDI."); // Cannot run NDI. Most likely because the CPU is not sufficient.
+	 return 0;
 	}
 
-	
-	// Create a finder
-	NDIlib_find_create_t NDI_find_create_desc; /* Defalt settings */
-	NDIlib_find_instance_t pNDI_find = NDIlib_find_create_v2(&NDI_find_create_desc);
-	if (!pNDI_find) return 0;
+	// Create a NDI finder	
+	pNDI_find = NDIlib_find_create_v2(&NDI_find_create_desc);
+	if (!pNDI_find) return 0; //error out if the NDI finder can't be created
 
-	// Wait until there is one source
-  uint32_t no_sources = 0;
-  const NDIlib_source_t* p_sources = NULL;
-  while (no_sources < 2){	// Wait until a source is found
-   printf("Looking for sources ...\n");
-   NDIlib_find_wait_for_sources(pNDI_find, 5000/* five seconds */);
-   p_sources = NDIlib_find_get_current_sources(pNDI_find, &no_sources);
+  std::string output_text; //preset file is temporary stored in this variable
+  std::ifstream preset_file("presets.txt"); //open the presets file
+  while(getline(preset_file, output_text)){
+   int stored = 0;
+   int receiver_id = 0;
+   const char* ndi_name = output_text.c_str();;
+   std::string ndi_string = ndi_name;
+   for(uint32_t i = 0; i < no_receivers; i++){
+    if(stored == 0){
+     if(ndi_running_name[i] == ""){ //empty string array - make sure it is empty before trying to start receiver
+      ndi_running_name[i] = ndi_string;
+      stored = 1;
+      receiver_id = i;
+     } 
+    }
+   }
+   p_receivers[receiver_id] = new receive_audio(ndi_name, "NDI_recv");
   }
-
-	// We need at least one source
-	if (!p_sources) return 0;
-  // Display all the sources.
-  printf("NDI sources (%u found).\n", no_sources);
-  for(uint32_t i = 0; i < no_sources; i++){
-   printf("%u. %s\n", i + 1, p_sources[i].p_ndi_name);
-  }
-  
-  //new receive_audio(p_sources[2]);
-  new receive_audio(p_sources[0]);
-  new receive_audio(p_sources[1]);
-  // Destroy the NDI finder. We needed to have access to the pointers to p_sources[0]
-  NDIlib_find_destroy(pNDI_find);
-  
-  struct mg_mgr mgr;                                
+                               
   mg_mgr_init(&mgr);
   mg_http_listen(&mgr, "ws://0.0.0.0:80", fn, NULL);   // Create WebSocket and HTTP connection
   for (;;) mg_mgr_poll(&mgr, 1000);  // Block forever
   /* keep running until the Ctrl+C */
   while(1){
-    printf("NDI sources (%s found).\n","Test");
    sleep(1);
   }
   
   exit (0);
+}
+
+std::string convertToString(char* a){
+  std::string s = a;
+  return s;
 }
