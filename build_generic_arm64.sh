bda28b87d14148215d4116fc4a9c9e5cca31db0e
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-21 23:18:43 -0500

    fixed build script

--- build_generic_arm64.sh
+++ build_generic_arm64.sh
@@ -11,5 +11,5 @@ fi
 cp "NDI Advanced SDK for Linux"/include/* include/
 cp "NDI Advanced SDK for Linux"/lib/aarch64-newtek-linux-gnu/* lib/
 
-g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/v4l2ndi main.cpp PixelFormatConverter.cpp -lndi -ldl -g -O2
+g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/ndi2jack ndi2jack.cpp mongoose.c mjson.c -lndi -ldl -ljack
 
