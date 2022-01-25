8a0f1df070306c505f39f9feeec67bf1c73da371
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-24 23:03:42 -0500

    fix package error

--- preinstall.sh
+++ preinstall.sh
@@ -3,17 +3,5 @@
 apt-get update
 
 #install prerequisites
-apt-get -y install --no-install-recommends \
-	build-essential \
-	libjack-jackd2-dev \
-	qjackctl \
-	jackd \	
-    avahi-daemon \
-    avahi-discover \
-    avahi-utils \
-    libssl-dev \
-    libconfig++-dev \
-	g++ \	
-    curl \
-    || exit 1
+apt-get -y install --no-install-recommends build-essential libjack-jackd2-dev qjackctl jackd avahi-daemon avahi-discover avahi-utils libssl-dev libconfig++-dev g++ curl
 
