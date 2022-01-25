e0bc75a8e52042e2c3d20663c23e2a392d0eb16a
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-24 22:57:19 -0500

    update build for Raspberry Pi 3

---
+++ build_rpi3_armhf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+if [ ! -d "lib" ]; then
+  mkdir lib
+fi
+
+cp "NDI SDK for Linux"/include/* include/
+cp "NDI SDK for Linux"/lib/arm-rpi3-linux-gnueabihf/* lib/
+
+g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/ndi2jack ndi2jack.cpp mongoose.c mjson.c -lndi -ldl -ljack
+g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/jack2ndi jack2ndi.cpp -lndi -ldl -ljack
