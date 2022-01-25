db5730616582e097765fd87ae81eee9fa28749c2
Luke Plassman <hpcomwin10insider@outlook.com>
2022-01-24 22:50:03 -0500

    updated build preinstall packages

--- preinstall.sh
+++ preinstall.sh
@@ -3,12 +3,17 @@
 apt-get update
 
 #install prerequisites
-apt-get -y install \
+apt-get -y install --no-install-recommends \
+	build-essential \
+	libjack-jack2-dev \
+	qjackctl \
+	jackd \	
     avahi-daemon \
     avahi-discover \
     avahi-utils \
     libssl-dev \
     libconfig++-dev \
+	g++ \	
     curl \
     || exit 1
 
