8f97d85c06873b1fddb61b043f25a2933856da55
Luke Plassman <lplassman@outlook.com>
2022-08-23 11:20:42 -0400

    added generic armhf build option

---
+++ build_generic_armhf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+if [ ! -d "lib" ]; then
+  mkdir lib
+fi
+
+cp "NDI Advanced SDK for Linux"/include/* include/
+cp "NDI Advanced SDK for Linux"/lib/arm-newtek-linux-gnueabihf/* lib/
+
+g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/ndi2jack ndi2jack.cpp mongoose.c mjson.c -lndi -ldl -ljack
+g++ -std=c++14 -pthread  -Wl,--allow-shlib-undefined -Wl,--as-needed -Iinclude/ -L lib -o build/jack2ndi jack2ndi.cpp -lndi -ldl -ljack
